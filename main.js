d66f7b03acb876b129c1c062b3293dd05266cf37
Nurane1346 <nuraneh01@gmail.com>
2024-02-04 21:48:17 +0400

    created file

---
+++ main.js
@@ -0,0 +1,29 @@
+// Task-01
+
+// let user=["room","class","scool"];
+// user[1]="Classified"
+// console.log(user);
+
+// Task-02
+
+// let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jdi'];
+// toplam=0;
+// for(i=0;i<arr.length;i++){
+// toplam+=arr[i]
+//     console.log(`Episod ${i + 4}: ${arr[i]}`);
+// }
+
+// Task-03
+
+
+// Task-04
+
+// let reqem=prompt("Zehmet olmasa ededleri daxil edin.");
+// let bolgu=reqem.split(",");
+// console.log(bolgu);'32, 31, 34, 36, 31'
+
+// Task-05
+
+// let metn = [" 32 ", " 31 ", " 34 ", " 36 ", " 31 "];
+// let result=metn.join(";")
+// console.log(result);
